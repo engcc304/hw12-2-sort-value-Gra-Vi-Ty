ae97b7a0b3a0f9ba8c14e424db78818f73bd6f8c
Gra-Vi-Ty <worakan223@gmail.com>
2023-10-11 21:07:24 +0700

    upppp

--- Homework/HW12/hw12-2-sort-value.cpp
+++ Homework/HW12/hw12-2-sort-value.cpp
@@ -60,3 +60,52 @@
         99.99 88.88 77.77 66.66
 
 */
+
+#include <stdio.h>
+#include <string.h>
+
+struct A {
+    char name[50] ;
+    float score ;
+} ;
+
+int main() {
+    int n = 4 ;
+    struct A group[n] ;
+    printf( "Student A\n" ) ;
+    printf( "Name : \n" ) ;
+    scanf( "%s", group[1].name ) ;
+    printf( "Score : \n" ) ;
+    scanf( "%f", &group[1].score ) ;
+
+    printf( "Student B\n" ) ;
+    printf( "Name : \n" ) ;
+    scanf( "%s", group[2].name ) ;
+    printf( "Score : \n" ) ;
+    scanf( "%f", &group[2].score ) ;
+
+    printf( "Student C\n" ) ;
+    printf( "Name : \n" ) ;
+    scanf( "%s", group[3].name ) ;
+    printf( "Score : \n" ) ;
+    scanf( "%f", &group[3].score ) ;
+
+    printf( "Student D\n" ) ;
+    printf( "Name : \n" ) ;
+    scanf( "%s", group[4].name ) ;
+    printf( "Score : \n" ) ;
+    scanf( "%f", &group[4].score ) ;
+
+    printf( "%4s %4s %4s %4s\n",
+            group[1].name,
+            group[2].name,
+            group[3].name,
+            group[4].name) ;
+    printf( "%4.2f %4.2f %4.2f %4.2f",
+            group[1].score,
+            group[2].score, 
+            group[3].score, 
+            group[4].score) ;
+    
+    return 0 ;
+}
